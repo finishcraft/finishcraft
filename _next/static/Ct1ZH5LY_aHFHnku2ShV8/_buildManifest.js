2ca9bd52de7b133e6809b7d06189824697c32783
finishcraft <garett@finishcraft.com>
2022-08-19 23:51:01 +0000

    Deploying to public from @ finishcraft/finishcraft@39a784dc7e9292a1fb0b0f039228931d6485756b 🚀

---
+++ _next/static/Ct1ZH5LY_aHFHnku2ShV8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b7fe99716af0c628.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
